d8d09bd443d4d4f6cd3940ada8817903303ef9aa
Anthony Sneed <tony@tonysneed.com>
2017-10-14 12:00:32 -0500

    Add project files.

---
+++ HelloHandleBars/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloHandleBars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
